74441136adf3a20f8e018a8c0915a5b834d08ade
Narendra Joke <106897476+Narendra-Joke@users.noreply.github.com>
2026-07-08 13:10:48 +0530

    removed some unnecessary lines
    Removed commented-out code for password encoding.

--- Backend-UOI/src/main/java/com/root/BackendUoiApplication.java
+++ Backend-UOI/src/main/java/com/root/BackendUoiApplication.java
@@ -8,7 +8,5 @@ public class BackendUoiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendUoiApplication.class, args);
-//		System.out.println(new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder().encode("Naru@1008"));
 	}
-
 }
